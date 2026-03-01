47060e1aea32797ea33144e60aa97d7d24cfe8ec
yvon <yvon.huynh@gmail.com>
2026-03-01 17:38:41 +0100

    add router systeme, createRouter,router-link ans router-view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
+import {ref} from 'vue'
 
 const clubName = "Tennis Club Vue"
 const today = new Date().toISOString().split('T')[0]
 
 // State global simple (à améliorer plus tard)
-const reservations = ref([])
+//const reservations = ref([])
 
 // TODO : passer reservations aux views via props si nécessaire
 </script>
@@ -13,7 +14,14 @@ const reservations = ref([])
   <div>
     <h1>{{ clubName }}</h1>
     <p>Date du jour : {{ today }}</p>
-
+    <nav>
+      <router-link to="/">Réserver</router-link> |
+      <router-link to="/reservations">Réservations</router-link> |
+      <router-link to="/contact">Contact</router-link>
+    </nav>
+    <router-view />
+    <hr />
+    <router-view />
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Contact from '../views/Contact.vue'
+import Reservations from '../views/Reservations.vue'
 
 
-const routes = [
-    { path: '/', component: Home }
-]
+// const routes = [
+//     { path: '/', component: Home },
+//     { path: '/', component: Contact }
+// ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+    routes: [
+        { path: '/', component: Home },
+        { path: '/reservations', component: Reservations },
+        { path: '/contact', component: Contact }
+
+    ],
+    history: createWebHistory()
 })
 
 export default router
